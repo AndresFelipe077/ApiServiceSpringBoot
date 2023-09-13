674b606ec62b22d39160c68e34c696aacc476aa4
AndresFelipe077 <pizoluligoa@gmail.com>
2023-09-12 19:38:51 -0500

    Best code by controllers of functions crud

--- src/main/java/com/example/crud_fast/controller/StudentController.java
+++ src/main/java/com/example/crud_fast/controller/StudentController.java
@@ -1,5 +1,41 @@
 package com.example.crud_fast.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.crud_fast.entity.Student;
+import com.example.crud_fast.service.StudentService;
+
+@RestController
+@RequestMapping(path = "api/v1/students")
 public class StudentController {
-    
+
+    @Autowired
+
+    private StudentService studentService;
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return studentService.getStudents();
+    }
+
+    @GetMapping("/{studentId}")
+    public Optional<Student> getStudentById(@PathVariable("studentId") Long studentId) {
+        return studentService.getStudent(studentId);
+    }
+
+    @PostMapping
+    public Student saveUpdate(@RequestBody Student student) {
+        studentService.saveOrUpdate(student);
+        return student;
+    }
+
+    @DeleteMapping("/{studentId}")
+    public void saveUpdate(@PathVariable("studentId") Long studentId) {
+        studentService.delete(studentId);
+    }
+
 }
